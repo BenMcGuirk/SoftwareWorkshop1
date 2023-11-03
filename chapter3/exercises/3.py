1454bd4e9ff276a27205e7ea4c78fed803cc38d4
Ben McGuirk <benmcguirk15@gmail.com>
2023-11-03 09:36:47 +0000

    additions

--- chapter3/exercises/3.py
+++ chapter3/exercises/3.py
@@ -1,3 +1,31 @@
 """
-
+Modify the guessing game of sectoin 3.5 so that the user thinks of a number that the computer has to guess. 
+The computer must make the minmum possible of guesses (implement binary search).
 """
+import math
+
+smaller = int(input("Enter the smaller number: "))
+larger = int(input("Enter the larger number: "))
+userNumber = int(input("Enter your number: "))
+count = 0
+
+#print(list(range(smaller, larger + 1))) testing array formation
+numArray = list(range(smaller, larger + 1))
+left = 0
+right = len(numArray) - 1
+
+
+while True:
+    count += 1
+    mid = math.floor((left + right) / 2)
+    computerNumber = numArray[mid]
+    print("Computer's guess is: " + str(computerNumber) + ".")
+    if computerNumber < userNumber:
+        print("TOO LOW!")
+        left = mid + 1
+    elif computerNumber > userNumber:
+        print("TOO HIGH")
+        right = mid
+    else:
+        print("CORRECT! The computer got it in " + str(count) + " tries.")
+        break
--- chapter3/exercises/4.py
+++ chapter3/exercises/4.py
@@ -1,3 +1,9 @@
+"""
+Write a program representing how far a bouncy ball has travelled. The 'bounciness' is determined by a ratio of its height 
+dropped against the height reached after bouncing. For example if its dropped from 10m and bounces 6m, its ratio is 0.6.
+Allow user to input the inital height and the number of times its allowed to bounce, and return total distance travelled 
+by the ball.
+"""
 height = int(input('Enter the height: '))
 numberOfBounces = int(input('Enter the number of bounces: '))
 totalDistance = 0
