3a77455968c904a9eac8186c95016aeff0ba8e68
Ben McGuirk <benmcguirk15@gmail.com>
2023-11-23 01:03:39 +0000

    additions

--- chapter4/exercises/4.py/decimalToOctal.py
+++ chapter4/exercises/4.py/decimalToOctal.py
@@ -1,4 +1,14 @@
 """
 Convert decimal to octal
 """
-decimal = float(input("Enter your decimal number: "))
+decimal = int(input("Enter your decimal number: "))
+octal = ''
+if decimal == 0:
+    print(0)
+else:
+    while decimal > 0:
+        remainder = decimal % 8
+        decimal = decimal / 8
+        decimal = round(decimal, 0)
+        octal = octal + str(int(remainder))
+print(octal)
--- chapter4/exercises/4.py/octalToDecimal.py
+++ chapter4/exercises/4.py/octalToDecimal.py
@@ -1,4 +1,10 @@
 """
 Convert octal to decimal
 """
-octal = input("Enter your octal number: ")
+octal = str(input("Enter your octal number: "))
+decimal = 0
+exponent = len(octal) - 1
+for x in octal:
+    decimal = decimal + int(x) * 8 ** exponent
+    exponent = exponent - 1
+print(decimal)
